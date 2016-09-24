28eee3879f06c1523b1cbacc0125906cbdb2d7fa
Rachel <rachelwshen@gmail.com>
2016-09-24 22:27:39 +0000

    updated the server to reflect flask app and added new function docstring

--- server.py
+++ server.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
-import geocoder
 from datetime import datetime
-from model import db, connect_to_db, Trip, Preference, TripPreference
+import model
 import json
-import googlemaps
 import pdb
+
+app = Flask(__name__)
+
+app.secret_key = "Shhhhh"
 
 @app.route('/')
 def homepage():
@@ -20,3 +22,11 @@ def login():
 
 def register():
     """Register the user"""
+
+def view_entries_by_tag():
+    """Create a function that sorts the entries by user's input tag"""
+
+if __name__ == "__main__":
+    DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(debug=True, host='0.0.0.0', port=5000) #vagrant requires port to be 5000
