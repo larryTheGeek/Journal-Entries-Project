6f9abf29a1822b4a471ac282bb19a8228518c76d
Rachel <rachelwshen@gmail.com>
2016-11-09 00:30:57 +0000

    having fun with click on the login, click modal and then click login the page goes blank

--- sample_data.sql
+++ sample_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username, password, email)
-VALUES (),
+VALUES ('fluffykitty', 'password123', 'youknowit@aol.com'),
 ();
 
 INSERT INTO entries (entry_date, entry_body, username, tag)
