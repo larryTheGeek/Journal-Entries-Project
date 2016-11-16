d46553cc96edb49875f3cedf0ade6bc1a64eadfc
Rachel <rachelwshen@gmail.com>
2016-11-16 21:25:14 +0000

    working on logout functionality for user

--- sample_data.sql
+++ sample_data.sql
@@ -3,8 +3,8 @@ VALUES (1, 'fluffykitty', 'password123', 'youknowit@aol.com'),
 (2, 'buddybrady', 'hellomate', 'buddybrady@gmail.com');
 
 INSERT INTO entries 
-VALUES (1, '2016-11-11 12:01AM', 'It was a good day and I felt like I could take on the world', 1),
-        (2, '2016-10-11 12:01PM', 'I made a good friend', 2);
+VALUES (1, '2016-11-11 12:01AM', 'Update on my day', 'It was a good day and I felt like I could take on the world', 1),
+        (2, '2016-10-11 12:01PM', 'Friendships Formed', 'I made a good friend', 2);
 
 -- INSERT INTO tags
 -- VALUES ()
